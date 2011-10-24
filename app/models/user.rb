43e99219db0c14b5047d48616c07f582337710fa
Matthew Liu <matthewliu@gmail.com>
2011-10-24 00:32:35 -0700

    Minor copy changes to v0.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   #Common regular expression for valid email addresses
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  validates :email_address, :presence => true, :format => { :with => email_regex }                      
+  validates :email_address, :presence => true, :format => { :with => email_regex }
 
 end
